77ee1a2be4f5d2bc428920f2ba40db19b6769b88
ohdeer31 <99162237+ohdeer31@users.noreply.github.com>
2023-04-05 16:30:11 -0700

    adding effects on navbar and certificate page

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import seanFace from "../images/daBoss.png";
 import * as bootstrap from "bootstrap";
 import "../style.css";
 window.bootstrap = bootstrap;
 
 function NavTabs({ currentPage, handlePageChange }) {
+  const lastScrollTop = useRef(0);
+  const [isNevbarVisible, setIsNavbarVisible] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener(
+      "scroll",
+      () => {
+        var { pageYOffset } = window;
+        if (pageYOffset > lastScrollTop.current) {
+          // downward scroll
+          setIsNavbarVisible(false);
+        } else if (pageYOffset < lastScrollTop.current) {
+          // upward scroll
+          setIsNavbarVisible(true);
+        } // else was horizontal scroll
+        lastScrollTop.current = pageYOffset <= 0 ? 0 : pageYOffset;
+      },
+      { passive: true }
+    );
+  }, []);
+
   return (
-    <div className="d-inline d-flex mx-3">
-      <div>
-        {/* <div className="container-fluid"> */}
+    // <div className="d-inline">
+    <div className={`scrollNav ${isNevbarVisible ? "visible" : ""}`}>
+      <div className="container-fluid">
         <a
           href="#homepage"
           onClick={() => handlePageChange("HomePage")}
           className={
-            currentPage === "HomePage"
-            // currentPage === "HomePage" ? "nav-link active" : "nav-link"
+            currentPage === "HomePage" ? "nav-link active" : "nav-link"
           }
         >
           <img
@@ -26,54 +46,52 @@ function NavTabs({ currentPage, handlePageChange }) {
           />
         </a>
       </div>
-      <div className="rightNav p-2">
-        <ul className="nav" style={{ fontSize: "20px" }}>
-          <li className="nav-item">
-            <a
-              href="#aboutus"
-              onClick={() => handlePageChange("AboutUs")}
-              className={
-                currentPage === "AboutUs" ? "nav-link active" : "nav-link"
-              }
-            >
-              AboutUs
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              href="#certificate"
-              onClick={() => handlePageChange("Certificate")}
-              className={
-                currentPage === "Certificate" ? "nav-link active" : "nav-link"
-              }
-            >
-              Certificate
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              href="#service"
-              onClick={() => handlePageChange("Service")}
-              className={
-                currentPage === "Service" ? "nav-link active" : "nav-link"
-              }
-            >
-              Service
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              href="#support"
-              onClick={() => handlePageChange("Support")}
-              className={
-                currentPage === "Support" ? "nav-link active" : "nav-link"
-              }
-            >
-              Support
-            </a>
-          </li>
-        </ul>
-      </div>
+      <ul className="nav-items" style={{ fontSize: "20px" }}>
+        <li className="nav-item">
+          <a
+            href="#aboutus"
+            onClick={() => handlePageChange("AboutUs")}
+            className={
+              currentPage === "AboutUs" ? "nav-link active" : "nav-link"
+            }
+          >
+            AboutUs
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#certificate"
+            onClick={() => handlePageChange("Certificate")}
+            className={
+              currentPage === "Certificate" ? "nav-link active" : "nav-link"
+            }
+          >
+            Certificate
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#service"
+            onClick={() => handlePageChange("Service")}
+            className={
+              currentPage === "Service" ? "nav-link active" : "nav-link"
+            }
+          >
+            Service
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#support"
+            onClick={() => handlePageChange("Support")}
+            className={
+              currentPage === "Support" ? "nav-link active" : "nav-link"
+            }
+          >
+            Support
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
--- src/components/pages/Certificate.js
+++ src/components/pages/Certificate.js
@@ -1,4 +1,5 @@
 import React from "react";
+import certificate from "../../images/certificate.png";
 
 export default function Certificate() {
   return (
@@ -6,8 +7,12 @@ export default function Certificate() {
       <header>
         <h1>This is certificate page</h1>
       </header>
-      <div>
-        <p></p>
+      <div className="d-flex justify-content-center certiPage">
+        <img src={certificate} alt="certificate" className="m-3"></img>
+        <div className="m-3">
+          <h3>IGS ISO Certificate</h3>
+          <h6>explain this</h6>
+        </div>
       </div>
     </div>
   );
